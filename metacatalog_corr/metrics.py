f2f6b6495fb2a14fe221e831f3ae012c6bd9732a
AlexDo1 <alex.dolich@gmail.com>
2021-12-02 23:35:44 +0100

    mutual info, conditional entropy calculation: return 0 if abs(value) is < 1e-4

--- metacatalog_corr/metrics.py
+++ metacatalog_corr/metrics.py
@@ -123,6 +123,9 @@ def conditional_entropy(left: np.ndarray, right: np.ndarray, **kwargs) -> float:
     """
     corr = 1 - skinfo.conditional_entropy(left, right, **kwargs)
 
+    # return 0 if the absolute value of conditional entropy is close to 0
+    if abs(corr) < 1e-4:
+        corr = 0
     return corr
 
 def mutual_information(left: np.ndarray, right: np.ndarray, **kwargs) -> float:
@@ -131,6 +134,9 @@ def mutual_information(left: np.ndarray, right: np.ndarray, **kwargs) -> float:
     """
     corr = skinfo.mutual_information(left, right, **kwargs)
 
+    # return 0 if the absolute value of mutual information is close to 0
+    if abs(corr) < 1e-4:
+        corr = 0
     return corr
 
 def jensen_shannon(left: np.ndarray, right: np.ndarray, **kwargs) -> float:
